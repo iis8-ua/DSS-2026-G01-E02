0f3151144fd15a5bfcae4a08f8071dc3145a3baa
jmac18-ua <jmac18@alu.ua.es>
2026-02-16 14:41:53 +0100

    Reserva casi terminada

---
+++ app/Models/Reserva.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids; // Importante para UUID
+
+class Reserva extends Model
+{
+    use HasFactory, HasUuids;
+
+    // ID no autoincremental
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['espacio_id', 'user_id', 'fecha_hora_inicio', 'fecha_hora_fin', 'estado'];
+
+    // Relación con el detalle grupal
+    public function reservaGrupal()
+    {
+        return $this->hasOne(ReservaGrupal::class, 'reserva_id');
+    }
+
+    // creador de la reserva
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
