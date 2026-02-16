cf447465c7017b9499976c51430ee59f1d97b2d9
iis8-ua <iis8@alu.ua.es>
2026-02-16 10:52:09 +0100

    Se ha elaborado la estructura de la tabla Espacio con la migracion

--- database/migrations/2026_02_16_090526_create_tipo_espacios_table.php
+++ database/migrations/2026_02_16_090526_create_tipo_espacios_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tipo_espacios', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string("nombre");
             $table->timestamps();
         });
--- database/migrations/2026_02_16_090617_create_espacios_table.php
+++ database/migrations/2026_02_16_090617_create_espacios_table.php
@@ -12,8 +12,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('espacios', function (Blueprint $table) {
-            $table->id();
+            $table->uuid()->primary();
+            $table->string('nombre');
+            $table->integer('aforo');
+            $table->string('estado')->default('HABILITADO');
+            $table->text('caracteristicas')->nullable();
             $table->timestamps();
+
+            //Para la relacion de la localizacion, ya que la clave primaria alli esta compuesta por estas tres
+            $table->float('loc_latitud');
+            $table->float('loc_longitud');
+            $table->integer('loc_piso');
+
+            $table->foreign(['loc_latitud', 'loc_longitud', 'loc_piso'])
+                ->references(['latitud', 'longitud', 'piso'])
+                ->on('localizacions')
+                ->onDelete('cascade');
+
+            //Relacion con el tipo espacio
+            $table->foreignUuid('tipo_espacio_id')->constrained('tipo_espacios');
         });
     }
 
