a05c2d45c28cfd92edab60cd6a8e2752abafe46b
iis8-ua <iis8@alu.ua.es>
2026-02-17 11:09:55 +0100

    Se ha elaborado el seeder de la localización con unos datos provisionales tambien

--- database/seeders/LocalizacionSeeder.php
+++ database/seeders/LocalizacionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Localizacion;
 
 class LocalizacionSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class LocalizacionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Localizacion::create(['latitud' => 40.5, 'longitud' => -3.5, 'piso' => 1]);
+        Localizacion::create(['latitud' => 40.5, 'longitud' => -3.5, 'piso' => 2]);
+        Localizacion::create(['latitud' => 41.0, 'longitud' => -2.0, 'piso' => 0]);
     }
 }
