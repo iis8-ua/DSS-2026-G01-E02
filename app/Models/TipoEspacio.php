8dc1be1cf5f341efd07786370b909138d29b7633
iis8-ua <iis8@alu.ua.es>
2026-02-17 10:16:44 +0100

    Se ha implementado el modelo de TipoEspacio con la relacion correspondiente hacia Espacio

--- app/Models/TipoEspacio.php
+++ app/Models/TipoEspacio.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class TipoEspacio extends Model
 {
-    //
+    use HasUuids; //esto lo que hace es activar la generacion auto de los uuid
+
+    //se pone para que se eviten errores, luego se ve si se puede suprimir
+    protected $table = 'tipo_espacios';
+
+    protected $fillable = ['nombre'];
+
+    /**
+     * Devuelve los espacios que tienen ese tipo de espacio asociado
+     * Relacion: Un tipo de espacio tiene muchos espacios
+     */
+    public function espacios()
+    {
+        return $this->hasMany(Espacio::class, 'tipo_espacio_id');
+    }
 }
