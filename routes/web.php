845481f49f5836ba129319cb04fd5f0819a61f36
jmac18-ua <jmac18@alu.ua.es>
2026-03-09 11:17:00 +0100

    Rutas de vistas Admin y Blog creadas

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,25 @@ Route::get('/catalogo', function () {
     return view('reservations', ['espacios' => $espacios]);
 })->name('espacios.catalogo');
 
+Route::get('/admin', function () {
+    return view('admin', [
+        'usuarios' => [],
+        'espacios' => [],
+        'reservas' => [],
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog', [
+        'incidencias' => [],
+    ]);
+});
+
 Route::view('/aviso-legal', 'aviso-legal')->name('legal.aviso');
 Route::view('/privacidad', 'privacidad')->name('legal.privacidad');
 Route::view('/accesibilidad', 'accesibilidad')->name('legal.accesibilidad');
 Route::view('/cookies', 'cookies')->name('legal.cookies');
+
+
+Route::resource('espacios', EspacioController::class);
+Route::resource('reservas', ReservaController::class);
