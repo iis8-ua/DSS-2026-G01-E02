4d59931249af2ebc17cc03c45e627aaf5cc0ed3a
amg504-ua <amg504@alu.ua.es>
2026-03-15 18:33:13 +0100

    cambios en web para la ruta y en el header

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Espacio;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/', function () {
     return view('main');
@@ -11,6 +12,9 @@ Route::get('/catalogo', function () {
     $espacios = Espacio::all();
     return view('reservations', ['espacios' => $espacios]);
 })->name('espacios.catalogo');
+
+Route::get('/perfil', [UsuarioController::class, 'perfil'])
+->middleware('auth')->name('usuario.perfil');
 
 Route::view('/aviso-legal', 'aviso-legal')->name('legal.aviso');
 Route::view('/privacidad', 'privacidad')->name('legal.privacidad');
