73878e51949feb8431a3d1486e89c275e0a7b3c2
iis8-ua <iis8@alu.ua.es>
2026-03-17 14:40:06 +0100

    Se ha aumentado el seeder de los espacios para tener mas para reservar.

--- database/seeders/EspacioSeeder.php
+++ database/seeders/EspacioSeeder.php
@@ -17,18 +17,25 @@ class EspacioSeeder extends Seeder
     {
         $horarios = Horario::all();
 
+        if ($horarios->isEmpty()) {
+            $this->command->error('No hay horarios en la base de datos. Ejecuta HorarioSeeder primero.');
+            return;
+        }
+
         $tipoLab         = TipoEspacio::where('nombre', 'Laboratorio')->first();
         $tipoAula        = TipoEspacio::where('nombre', 'Aula Teoría')->first();
         $tipoDespacho    = TipoEspacio::where('nombre', 'Despacho')->first();
         $tipoInformatica = TipoEspacio::where('nombre', 'Aula Informática')->first();
         $tipoSalaEstudio = TipoEspacio::where('nombre', 'Sala de Estudio')->first();
 
+        $tipoDeportes    = TipoEspacio::firstOrCreate(['nombre' => 'Instalación Deportiva']);
+
+        // Localizaciones originales
         $locOriginal   = Localizacion::where('latitud', 40.5)->where('longitud', -3.5)->where('piso', 2)->first();
         $locPlantaBaja = Localizacion::where('latitud', 40.5)->where('longitud', -3.5)->where('piso', 0)->first();
         $locFacultad   = Localizacion::where('latitud', 40.501)->where('longitud', -3.502)->where('piso', 1)->first();
         $locBiblioteca = Localizacion::where('latitud', 40.505)->where('longitud', -3.495)->where('piso', 1)->first();
         $locAulario    = Localizacion::where('latitud', 41.0)->where('longitud', -2.0)->where('piso', 1)->first();
-
 
         if ($tipoLab && $locOriginal) {
             Espacio::create([
@@ -56,8 +63,8 @@ class EspacioSeeder extends Seeder
                 'loc_latitud'     => $locPlantaBaja->latitud,
                 'loc_longitud'    => $locPlantaBaja->longitud,
                 'loc_piso'        => $locPlantaBaja->piso,
-                'horario_inicio'  => $horarios[1]->inicio,
-                'horario_fin'     => $horarios[1]->fin,
+                'horario_inicio'  => $horarios->count() > 1 ? $horarios[1]->inicio : $horarios[0]->inicio,
+                'horario_fin'     => $horarios->count() > 1 ? $horarios[1]->fin : $horarios[0]->fin,
                 'imagen'          => 'aula-magna.jpg'
             ]);
         }
@@ -72,8 +79,8 @@ class EspacioSeeder extends Seeder
                 'loc_latitud'     => $locAulario->latitud,
                 'loc_longitud'    => $locAulario->longitud,
                 'loc_piso'        => $locAulario->piso,
-                'horario_inicio'  => $horarios[2]->inicio,
-                'horario_fin'     => $horarios[2]->fin,
+                'horario_inicio'  => $horarios->count() > 2 ? $horarios[2]->inicio : $horarios[0]->inicio,
+                'horario_fin'     => $horarios->count() > 2 ? $horarios[2]->fin : $horarios[0]->fin,
                 'imagen'          => 'aula-info.jpg'
             ]);
         }
@@ -88,8 +95,8 @@ class EspacioSeeder extends Seeder
                 'loc_latitud'     => $locBiblioteca->latitud,
                 'loc_longitud'    => $locBiblioteca->longitud,
                 'loc_piso'        => $locBiblioteca->piso,
-                'horario_inicio'  => $horarios[3]->inicio,
-                'horario_fin'     => $horarios[3]->fin,
+                'horario_inicio'  => $horarios->count() > 3 ? $horarios[3]->inicio : $horarios[0]->inicio,
+                'horario_fin'     => $horarios->count() > 3 ? $horarios[3]->fin : $horarios[0]->fin,
                 'imagen'          => 'sala-estudio.jpg'
             ]);
         }
@@ -104,9 +111,93 @@ class EspacioSeeder extends Seeder
                 'loc_latitud'     => $locFacultad->latitud,
                 'loc_longitud'    => $locFacultad->longitud,
                 'loc_piso'        => $locFacultad->piso,
-                'horario_inicio'  => $horarios[4]->inicio,
-                'horario_fin'     => $horarios[4]->fin,
+                'horario_inicio'  => $horarios->count() > 4 ? $horarios[4]->inicio : $horarios[0]->inicio,
+                'horario_fin'     => $horarios->count() > 4 ? $horarios[4]->fin : $horarios[0]->fin,
                 'imagen'          => 'despacho-tutorias.jpg'
+            ]);
+        }
+
+        $nuevosEspacios = [
+            [
+                'nombre' => 'Gimnasio', 'aforo' => 100, 'estado' => 'DESHABILITADO',
+                'caracteristicas' => 'Material sintético, de 33 x 13 m, altura de 3,5 m. Gimnasio de musculación con todo tipo de aparatos...',
+                'imagen' => '1773753644_gym-ua.jpg', 'lat' => 40.5, 'lon' => -3.5, 'piso' => -1
+            ],
+            [
+                'nombre' => 'Campo de fútbol', 'aforo' => 22, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Hierba artificial, de 120 x 65 m, con iluminación. 1 campo de fútbol 11 / rugby, o 2 de fútbol 8',
+                'imagen' => '1773753885_futbol-ua.jpg', 'lat' => 40.498, 'lon' => -3.508, 'piso' => 0
+            ],
+            [
+                'nombre' => 'Campo de hockey', 'aforo' => 12, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Hierba artificial, de 90 x 55 m, con iluminación',
+                'imagen' => '1773753972_hockey-ua.jpg', 'lat' => 40.501, 'lon' => -3.502, 'piso' => 0
+            ],
+            [
+                'nombre' => 'Pista semicubierta', 'aforo' => 10, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Material sintético, de 44 x 22 m, con iluminación. Fútbol sala, baloncesto, balonmano y hockey sala',
+                'imagen' => '1773754042_semicubierta-ua.jpg', 'lat' => 41.0, 'lon' => -2.0, 'piso' => 0
+            ],
+            [
+                'nombre' => 'Pista de pádel', 'aforo' => 4, 'estado' => 'HABILITADO',
+                'caracteristicas' => '3 pistas de hierba artificial, de 20 x 10 m, con iluminación',
+                'imagen' => '1773754091_padel-ua.jpg', 'lat' => 40.505, 'lon' => -3.495, 'piso' => 0
+            ],
+            [
+                'nombre' => 'Pista de tenis', 'aforo' => 4, 'estado' => 'HABILITADO',
+                'caracteristicas' => '3 pistas de hormigón pulido, de 36 x 18 m, con iluminación',
+                'imagen' => '1773754160_tenis-ua.jpg', 'lat' => 40.5, 'lon' => -3.5, 'piso' => 1
+            ],
+            [
+                'nombre' => 'Pista de atletismo', 'aforo' => 6, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Material sintético, de 400 m y 8 calles, con iluminación',
+                'imagen' => '1773754256_atletismo-ua.jpg', 'lat' => 40.505, 'lon' => -3.495, 'piso' => -1
+            ],
+            [
+                'nombre' => 'Pista de baloncesto', 'aforo' => 10, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Material sintético, de 32 x 16 m, con iluminación',
+                'imagen' => '1773754309_baloncesto-ua.jpg', 'lat' => 40.498, 'lon' => -3.508, 'piso' => 1
+            ],
+            [
+                'nombre' => 'Pista de voley playa', 'aforo' => 4, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Arena de sílice, de 24 x 12 m, con iluminación',
+                'imagen' => '1773754372_voley-playa-ua.jpg', 'lat' => 40.501, 'lon' => -3.502, 'piso' => 2
+            ],
+            [
+                'nombre' => 'Vestuarios semicubierta', 'aforo' => 25, 'estado' => 'HABILITADO',
+                'caracteristicas' => '16 vestuarios: 6 de 52,50 m2, 4 de 120,00 m2, 4 de 12,50 m2 y 2 de 6,55 m2 (discapacitados)',
+                'imagen' => '1773754430_vestuarios-semicubierta-ua.jpg', 'lat' => 40.5, 'lon' => -3.5, 'piso' => 4
+            ],
+            [
+                'nombre' => 'Circuito natural', 'aforo' => 200, 'estado' => 'HABILITADO',
+                'caracteristicas' => 'Pista de arena de unos 700 m y aparatos de gimnasia',
+                'imagen' => '1773754490_circuito-natural-ua.jpg', 'lat' => 40.501, 'lon' => -3.502, 'piso' => 3
+            ]
+        ];
+
+        // Recorremos el array y creamos las instalaciones
+        foreach ($nuevosEspacios as $index => $datos) {
+
+            $loc = Localizacion::firstOrCreate([
+                'latitud'  => $datos['lat'],
+                'longitud' => $datos['lon'],
+                'piso'     => $datos['piso']
+            ]);
+
+            $horario = $horarios[$index % $horarios->count()];
+
+            Espacio::create([
+                'nombre'          => $datos['nombre'],
+                'aforo'           => $datos['aforo'],
+                'estado'          => $datos['estado'],
+                'caracteristicas' => $datos['caracteristicas'],
+                'tipo_espacio_id' => $tipoDeportes->id,
+                'loc_latitud'     => $loc->latitud,
+                'loc_longitud'    => $loc->longitud,
+                'loc_piso'        => $loc->piso,
+                'horario_inicio'  => $horario->inicio,
+                'horario_fin'     => $horario->fin,
+                'imagen'          => $datos['imagen']
             ]);
         }
     }
