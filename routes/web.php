9132ba0a20d29a8f9f289355e64d159cc43b5b47
iis8-ua <iis8@alu.ua.es>
2026-04-20 10:51:24 +0200

    Se ha protegido las rutas de las URL para controlar los accesos dependiendo el tipo de usuario.

--- routes/web.php
+++ routes/web.php
@@ -15,72 +15,61 @@ use App\Http\Controllers\IncidenciaController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\HorarioController;
 
-Route::get('/', [MapController::class, 'map'])
-    ->middleware('auth')
-    ->name('inicio');
 
-Route::post('/notificacion/{id}/view', [NotificationController::class, 'viewNotification'])
-    ->name('notification.view');
-Route::post('/notificacion/viewall', [NotificationController::class, 'viewAllNotificationsAsUser'])
-    ->name('notification.viewall');
-
-Route::get('/catalogo', [EspacioController::class, 'catalogo'])->name('espacios.catalogo');
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-
-Route::get('/perfil', [UsuarioController::class, 'perfil'])
-    ->middleware('auth')
-    ->name('perfil');
-
-Route::get('/mis-reservas', [ReservaController::class, 'misReservas'])
-    ->middleware('auth')
-    ->name('reservas.mias');
-
-Route::view('/login', 'login')
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/login', [LoginController::class, 'login'])
-    ->middleware('guest');
-
-Route::post('/logout', [LoginController::class, 'logout'])
-    ->middleware('auth')
-    ->name('logout');
-
-Route::get('/reservas/nueva/{espacio}', [ReservaController::class, 'nueva'])
-    ->middleware('auth')
-    ->name('reservas.nueva');
-
-Route::post('/reservas/nueva/{espacio}', [ReservaController::class, 'guardarNueva'])
-    ->middleware('auth')
-    ->name('reservas.guardarNueva');
-
+//Rutas que son publicas
+Route::view('/login', 'login')->middleware('guest')->name('login');
+Route::post('/login', [LoginController::class, 'login'])->middleware('guest');
 Route::view('/aviso-legal', 'aviso-legal')->name('legal.aviso');
 Route::view('/privacidad', 'privacidad')->name('legal.privacidad');
 Route::view('/accesibilidad', 'accesibilidad')->name('legal.accesibilidad');
 Route::view('/cookies', 'cookies')->name('legal.cookies');
+Route::get('/catalogo', [EspacioController::class, 'catalogo'])->name('espacios.catalogo');
 
-Route::patch('/reservas/{reserva}/aprobar', [ReservaController::class, 'aprobar'])->name('reservas.aprobar');
-Route::patch('/reservas/{reserva}/rechazar', [ReservaController::class, 'rechazar'])->name('reservas.rechazar');
 
-Route::resource('espacios', EspacioController::class);
-Route::resource('tipos-espacio', TipoEspacioController::class);
-Route::resource('localizaciones', LocalizacionController::class);
-Route::resource('horarios', HorarioController::class);
-Route::resource('incidencias', IncidenciaController::class);
+//Rutas para cualquier usuario que esta iniciado sesion
+Route::middleware(['auth', 'es.usuario'])->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::resource('notificaciones', NotificationController::class)->parameters([
-    'notificaciones' => 'notificacion'
-]);
+    Route::get('/', [MapController::class, 'map'])->name('inicio');
 
-Route::resource('reservas-grupales', ReservaGrupalController::class)->parameters([
-    'reservas-grupales' => 'reservasGrupal'
-]);
+    Route::get('/perfil', [UsuarioController::class, 'perfil'])->name('perfil');
+    Route::get('/perfil/{usuario}/editar', [UsuarioController::class, 'editPerfil'])->name('usuario.edit-perfil');
+    Route::put('/perfil/{usuario}/actualizar', [UsuarioController::class, 'updatePerfil'])->name('usuario.update-perfil');
 
-Route::resource('reservas', ReservaController::class);
-Route::resource('usuarios', UsuarioController::class);
-Route::get('/perfil/{usuario}/editar', [UsuarioController::class, 'editPerfil'])->name('usuario.edit-perfil');
-Route::put('/perfil/{usuario}/actualizar', [UsuarioController::class, 'updatePerfil'])->name('usuario.update-perfil');
+    Route::post('/notificacion/{id}/view', [NotificationController::class, 'viewNotification'])->name('notification.view');
+    Route::post('/notificacion/viewall', [NotificationController::class, 'viewAllNotificationsAsUser'])->name('notification.viewall');
 
-Route::get('/gestor/reservas/pendientes', [GestorEspaciosController::class, 'pendientes'])->name('gestor.reservas.pendientes');
-Route::patch('/gestor/reservas/{reserva}/aceptar', [GestorEspaciosController::class, 'aceptar'])->name('gestor.reservas.aceptar');
-Route::patch('/gestor/reservas/{reserva}/rechazar', [GestorEspaciosController::class, 'rechazar'])->name('gestor.reservas.rechazar');
+    Route::get('/mis-reservas', [ReservaController::class, 'misReservas'])->name('reservas.mias');
+    Route::get('/reservas/nueva/{espacio}', [ReservaController::class, 'nueva'])->name('reservas.nueva');
+    Route::post('/reservas/nueva/{espacio}', [ReservaController::class, 'guardarNueva'])->name('reservas.guardarNueva');
+});
+
+
+//Rutas que son solo para admin
+Route::middleware(['auth', 'es.admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
+    Route::resource('espacios', EspacioController::class);
+    Route::resource('tipos-espacio', TipoEspacioController::class);
+    Route::resource('localizaciones', LocalizacionController::class);
+    Route::resource('horarios', HorarioController::class);
+    Route::resource('incidencias', IncidenciaController::class);
+    Route::resource('notificaciones', NotificationController::class)->parameters([
+        'notificaciones' => 'notificacion'
+    ]);
+    Route::resource('reservas-grupales', ReservaGrupalController::class)->parameters([
+        'reservas-grupales' => 'reservasGrupal'
+    ]);
+    Route::resource('reservas', ReservaController::class);
+    Route::resource('usuarios', UsuarioController::class);
+
+    Route::patch('/reservas/{reserva}/aprobar', [ReservaController::class, 'aprobar'])->name('reservas.aprobar');
+    Route::patch('/reservas/{reserva}/rechazar', [ReservaController::class, 'rechazar'])->name('reservas.rechazar');
+});
+
+//Rutas que son para el gestor de espacios
+Route::middleware(['auth', 'es.gestor'])->group(function () {
+    Route::get('/gestor/reservas/pendientes', [GestorEspaciosController::class, 'pendientes'])->name('gestor.reservas.pendientes');
+    Route::patch('/gestor/reservas/{reserva}/aceptar', [GestorEspaciosController::class, 'aceptar'])->name('gestor.reservas.aceptar');
+    Route::patch('/gestor/reservas/{reserva}/rechazar', [GestorEspaciosController::class, 'rechazar'])->name('gestor.reservas.rechazar');
+});
