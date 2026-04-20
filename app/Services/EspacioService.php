06d89592973edab9738511e5d921f3830c5971f2
iis8-ua <iis8@alu.ua.es>
2026-04-20 10:14:12 +0200

    Se ha protegido las transacciones del espacio como se ha visto en clase.

--- app/Services/EspacioService.php
+++ app/Services/EspacioService.php
@@ -2,13 +2,24 @@
 
 namespace App\Services;
 
-class EspacioService
-{
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
+use App\Models\Espacio;
+use Illuminate\Support\Facades\DB;
+
+class EspacioService {
+
+    public static function crear(array $datos, array $horariosIds): Espacio {
+        return DB::transaction(function () use ($datos, $horariosIds) {
+            $espacio = Espacio::create($datos);
+            $espacio->horario()->sync($horariosIds);
+            return $espacio;
+        });
+    }
+
+    public static function actualizar(Espacio $espacio, array $datos, array $horariosIds): Espacio {
+        return DB::transaction(function () use ($espacio, $datos, $horariosIds) {
+            $espacio->update($datos);
+            $espacio->horario()->sync($horariosIds);
+            return $espacio;
+        });
     }
 }
