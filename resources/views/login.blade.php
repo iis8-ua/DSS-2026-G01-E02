e6b3e92c00de124c940de558f4b3a521fede0700
jmac18-ua <jmac18@alu.ua.es>
2026-05-04 10:11:20 +0200

    Cambio menú

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,6 +20,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
