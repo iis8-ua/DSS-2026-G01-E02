223eeac0970217df0facb7c2027c4bd0bb3f49f0
iis8-ua <iis8@alu.ua.es>
2026-05-04 09:24:18 +0200

    Salto de linea.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,6 +40,7 @@
                                 placeholder="Introduce tu contraseña"
                                 required
                             >
+                            <br>
                             <a href="{{ route('password.request') }}" class="text-muted">¿Has olvidado tu contraseña?</a>
                         </div>
 
